5072df08f24423daab539b33fd36f2d293b0d98a
Murali Shankar <mshankar@slac.stanford.edu>
2023-03-22 16:19:37 -0700

    Add a new CompositeAuth request auth scheme.
    This is just an if statement that uses an appropriate endpoint
    and auth scheme based on examination of the environment.

--- krtc/__init__.py
+++ krtc/__init__.py
@@ -1,4 +1,5 @@
 from .krtc import KerberosTicket
+from .cmpauth import CompositeAuth
 from .version import __version__  # noqa: F401
 
-__all__ = ["KerberosTicket"]
+__all__ = ["KerberosTicket", "CompositeAuth"]
